a1c51d11de38c7dbf8052bcaa809d1cd6b468ca1
Daniel Wayota <danielglez1990@gmail.com>
2019-06-19 16:23:25 +0100

    Removed namespace from copypaste

--- Assets/Scripts/Population.cs
+++ Assets/Scripts/Population.cs
@@ -1,154 +1,151 @@
 using CerebroML.Genetics;
 using UnityEngine;
 
-namespace Done
+public class Population : MonoBehaviour
 {
-    public class Population : MonoBehaviour
+    [Header("Stuff")]
+    private WallSpawner wallSpawner;
+
+    [Header("Runners stuff")]
+    public GameObject runnerPrefab;
+    public int targetCoins = 8;
+
+    [Header("Population")]
+    public int populationSize = 8;
+    public float maxGenerationTime = 10f;
+
+    [Header("Map points")]
+    public Transform startPoint;
+
+    private Runner[] runnerPopulation;
+
+    private float checkTime = 0;
+    private float checkTimeOut = 1;
+    private float currentGenerationTime = 0;
+
+    private int currentGeneration;
+
+
+    // ========================================================
+    void Start()
     {
-        [Header("Stuff")]
-        private WallSpawner wallSpawner;
+        this.runnerPopulation = new Runner[this.populationSize];
 
-        [Header("Runners stuff")]
-        public GameObject runnerPrefab;
-        public int targetCoins = 8;
-
-        [Header("Population")]
-        public int populationSize = 8;
-        public float maxGenerationTime = 10f;
-
-        [Header("Map points")]
-        public Transform startPoint;
-
-        private Runner[] runnerPopulation;
-
-        private float checkTime = 0;
-        private float checkTimeOut = 1;
-        private float currentGenerationTime = 0;
-
-        private int currentGeneration;
-
-
-        // ========================================================
-        void Start()
+        for (int i = 0; i < this.populationSize; i++)
         {
-            this.runnerPopulation = new Runner[this.populationSize];
+            GameObject e = Instantiate(this.runnerPrefab, this.startPoint.position, Quaternion.identity);
+            e.transform.SetParent(this.transform);
 
-            for (int i = 0; i < this.populationSize; i++)
-            {
-                GameObject e = Instantiate(this.runnerPrefab, this.startPoint.position, Quaternion.identity);
-                e.transform.SetParent(this.transform);
-
-                this.runnerPopulation[i] = e.GetComponent<Runner>();
-            }
-            this.currentGeneration = 1;
-            // this.statusWindow.SetGenerationNumber(this.currentGeneration);
+            this.runnerPopulation[i] = e.GetComponent<Runner>();
         }
+        this.currentGeneration = 1;
+        // this.statusWindow.SetGenerationNumber(this.currentGeneration);
+    }
 
-        // ========================================================
-        void Update()
+    // ========================================================
+    void Update()
+    {
+        this.checkTime += Time.deltaTime;
+
+        if (this.checkTime > this.checkTimeOut)
         {
-            this.checkTime += Time.deltaTime;
+            this.checkTime -= this.checkTimeOut;
 
-            if (this.checkTime > this.checkTimeOut)
-            {
-                this.checkTime -= this.checkTimeOut;
-
-                if (this.IsEveryRunnerDead())
-                {
-                    this.AdvanceGeneration();
-                    this.currentGenerationTime = 0;
-                }
-            }
-
-            this.currentGenerationTime += Time.deltaTime;
-
-            if (this.currentGenerationTime > this.maxGenerationTime)
+            if (this.IsEveryRunnerDead())
             {
                 this.AdvanceGeneration();
-                this.currentGenerationTime -= this.maxGenerationTime;
+                this.currentGenerationTime = 0;
             }
         }
 
-        // ========================================================
-        bool IsEveryRunnerDead()
+        this.currentGenerationTime += Time.deltaTime;
+
+        if (this.currentGenerationTime > this.maxGenerationTime)
         {
-            bool allDead = true;
-
-            for (int i = 0; i < this.runnerPopulation.Length; i++)
-            {
-                if (!this.runnerPopulation[i].isDead)
-                {
-                    allDead = false;
-                }
-            }
-            return allDead;
+            this.AdvanceGeneration();
+            this.currentGenerationTime -= this.maxGenerationTime;
         }
+    }
 
-        // ========================================================
-        void AdvanceGeneration()
+    // ========================================================
+    bool IsEveryRunnerDead()
+    {
+        bool allDead = true;
+
+        for (int i = 0; i < this.runnerPopulation.Length; i++)
         {
-            this.wallSpawner.Restart();
-            this.currentGeneration++;
-            // this.statusWindow.SetGenerationNumber(this.currentGeneration);
-
-            // Generate new population
-            Genome[] newGenomes = new Genome[this.populationSize];
-            Genome[] parents = new Genome[2];
-
-            for (int i = 0; i < this.populationSize; i++)
+            if (!this.runnerPopulation[i].isDead)
             {
-                int parentCount = 0;
-
-                int safety = 0;
-                while (parentCount < 2 && safety < 10000)
-                {
-                    safety++;
-                    int index = Random.Range(0, this.populationSize);
-                    Runner candidate = this.runnerPopulation[index];
-                    float dice = Random.Range(0f, 1f);
-
-                    // Calculate fitness
-                    if (this.CalculateRunnerFitness(candidate) > dice)
-                    {
-                        parents[parentCount] = candidate.brain.GetGenome();
-                        parentCount++;
-                    }
-                }
-
-                if (parents.Length == 2)
-                {
-                    // Crossover
-                    Genome g1 = parents[0];
-                    Genome g2 = parents[1];
-
-                    Genome offspring = Genome.Crossover(g1, g2);
-                    offspring.Mutate(0.05f, 10f);
-
-                    newGenomes[i] = offspring;
-                }
-                else
-                {
-                    Debug.LogError("No parents found!");
-                }
-            }
-
-            // Hail to the new population!
-            for (int i = 0; i < this.populationSize; i++)
-            {
-                this.runnerPopulation[i].brain.SetGenome(newGenomes[i]);
-                this.runnerPopulation[i].Restart(this.startPoint.position);
+                allDead = false;
             }
         }
+        return allDead;
+    }
 
-        // ========================================================
-        float CalculateRunnerFitness(Runner e)
+    // ========================================================
+    void AdvanceGeneration()
+    {
+        this.wallSpawner.Restart();
+        this.currentGeneration++;
+        // this.statusWindow.SetGenerationNumber(this.currentGeneration);
+
+        // Generate new population
+        Genome[] newGenomes = new Genome[this.populationSize];
+        Genome[] parents = new Genome[2];
+
+        for (int i = 0; i < this.populationSize; i++)
         {
-            float points = (e.aliveTime / this.currentGenerationTime) * 0.75f;
+            int parentCount = 0;
 
-            float coinPoints = Mathf.Clamp01(e.coins / (float)this.targetCoins) * 0.25f;
-            points += coinPoints;
+            int safety = 0;
+            while (parentCount < 2 && safety < 10000)
+            {
+                safety++;
+                int index = Random.Range(0, this.populationSize);
+                Runner candidate = this.runnerPopulation[index];
+                float dice = Random.Range(0f, 1f);
 
-            return Mathf.Pow(points, 2);
+                // Calculate fitness
+                if (this.CalculateRunnerFitness(candidate) > dice)
+                {
+                    parents[parentCount] = candidate.brain.GetGenome();
+                    parentCount++;
+                }
+            }
+
+            if (parents.Length == 2)
+            {
+                // Crossover
+                Genome g1 = parents[0];
+                Genome g2 = parents[1];
+
+                Genome offspring = Genome.Crossover(g1, g2);
+                offspring.Mutate(0.05f, 10f);
+
+                newGenomes[i] = offspring;
+            }
+            else
+            {
+                Debug.LogError("No parents found!");
+            }
         }
+
+        // Hail to the new population!
+        for (int i = 0; i < this.populationSize; i++)
+        {
+            this.runnerPopulation[i].brain.SetGenome(newGenomes[i]);
+            this.runnerPopulation[i].Restart(this.startPoint.position);
+        }
+    }
+
+    // ========================================================
+    float CalculateRunnerFitness(Runner e)
+    {
+        float points = (e.aliveTime / this.currentGenerationTime) * 0.75f;
+
+        float coinPoints = Mathf.Clamp01(e.coins / (float)this.targetCoins) * 0.25f;
+        points += coinPoints;
+
+        return Mathf.Pow(points, 2);
     }
 }
